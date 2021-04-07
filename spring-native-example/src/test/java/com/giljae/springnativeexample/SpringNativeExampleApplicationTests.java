fbb6e73ebaef57d0ad05308df098a9c4084ba08b
Giljae Joo <giljae@gmail.com>
2021-04-07 14:15:04 +0900

    init spring-native

---
+++ spring-native-example/src/test/java/com/giljae/springnativeexample/SpringNativeExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giljae.springnativeexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNativeExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
