2e1b96357a6d52f2649165964ad2758b584f1f36
Giljae Joo <giljae@gmail.com>
2021-04-08 11:30:27 +0900

    added

--- spring-native-example/src/main/java/com/giljae/springnativeexample/SpringNativeExampleApplication.java
+++ spring-native-example/src/main/java/com/giljae/springnativeexample/SpringNativeExampleApplication.java
@@ -2,6 +2,14 @@ package com.giljae.springnativeexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @SpringBootApplication
 public class SpringNativeExampleApplication {
@@ -10,4 +18,10 @@ public class SpringNativeExampleApplication {
 		SpringApplication.run(SpringNativeExampleApplication.class, args);
 	}
 
+	@Bean
+	RouterFunction<ServerResponse> routes() {
+		return route()
+				.GET("/", request -> ok().body(Mono.just("Spring Native Example!!!"), String.class))
+				.build();
+	}
 }
--- spring-native-example/src/test/java/com/giljae/springnativeexample/SpringNativeExampleApplicationTests.java
+++ spring-native-example/src/test/java/com/giljae/springnativeexample/SpringNativeExampleApplicationTests.java
@@ -1,13 +1,25 @@
 package com.giljae.springnativeexample;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class SpringNativeExampleApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private WebTestClient webClient;
 
+	@Test
+	void getRootReturn() {
+		webClient
+				.get().uri("/")
+				.exchange()
+				.expectStatus().is2xxSuccessful()
+				.expectBody(String.class).isEqualTo("Spring Native Example!!!");
+	}
 }
